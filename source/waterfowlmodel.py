d4ecca0b58b525faa8aa3dc488e94eba9b932293
Michael Mitchell <mmitchell@ducks.org>
2019-11-14 12:21:59 -0500

    Added more documentation and filled out model structure.  Added conda environment file

--- source/waterfowlmodel.py
+++ source/waterfowlmodel.py
@@ -1,15 +1,57 @@
 """
-Waterfowl model header at the top.
+Module Waterfowl
+================
+Defines Waterfowlmodel class which is initialized by supplying an area of interest shapfile, wetland shapefile, kilocalorie by habitat type table, and the table linking the wetland shapefile to the kcal table.
 """
 class Waterfowlmodel:
   """Class to store waterfowl model parameters."""
-  def __init__(self, aoi):
-    """Create a waterfowl model object by providing an area of interst (aoi)."""
+  def __init__(self, aoi, nwi, kcalTable, crosswalk):
+    """
+    Creates a waterfowl model object.
+    
+    :param aoi: Area of interest shapefile
+    :type aoi: str
+    :param nwi: National Wetlands Inventory shapefile
+    :type nwi: str
+    :param kcalTable: CSV file containing two columns [habitat type, kilocalorie value by acre]
+    :type kcalTable: str
+    :param crosswalk: CSV file relating wetland habitat types to kcal csv table
+    :type crosswalk: str
+    """
     self.aoi = aoi
+    self.nwi = nwi
+    self.kcalTbl = kcalTable
+    self.crossTbl = crosswalk
 
   def getAOI(self):
     """
+    Returns the area of interest.  For testing purposes
+
     :return: The model area of interest
     :rtype: str
     """
-    return self.aoi 
+    return self.aoi
+
+  def dstOutout(self):
+    """
+    Runs energy difference between NAWCA stepdown objectives and available habitat.
+
+    :return: Shapefile containing model results at the county level
+    :rtype: str
+    """
+    return "outputFile"
+
+  def bin(self, aggData, aggCol, bins):
+    """
+    Calculates proportional sum aggregate based on area within specified columns of a given dataset to features from another dataset.
+
+    :param aggData: Dataset that contains information to be aggregated spatially
+    :type aggData: str
+    :param aggCol: Columns within aggData that need to have the sum aggregate function applied to them
+    :type aggCol: str
+    :param bins: Spatial dataset used as the aggregation feature.  Data will be binned to the features within this dataset.
+    :type bins: str
+    :return: Spatial Sum aggregate of aggData within supplied bins
+    :rtype:  str
+    """
+    return "binned file"
