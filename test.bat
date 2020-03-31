59e734803fef197015f7e1e7587ee6fb2fca10ca
jeskill <jskillman@ducks.org>
2020-03-31 11:22:11 -0400

    Added line so Jes can run this bat file on her local computer.

--- test.bat
+++ test.bat
@@ -1,2 +1,4 @@
 @ECHO OFF
-python runModel.py -w "D:\GIS\scratch\dst" -g ModelReady.gdb -l testnwi aoiWetland.json -k kcal.csv -d EnergyDemand  -a testJV -b HUC12
+: python runModel.py -w "D:\GIS\scratch\dst" -g ModelReady.gdb -l testnwi aoiWetland.json -k kcal.csv -d EnergyDemand  -a testJV -b HUC12
+
+python D:\bitbucket_rename\abdu_dst\runModel.py -w "C:\Users\jskillman\Ducks Unlimited Incorporated\GIS Department - ABDU DST and more!\GIS\ModelReady" -g ModelReady.gdb -l testnwi aoiWetland.json -k kcal.csv -d EnergyDemand  -a testJV -b huc12
