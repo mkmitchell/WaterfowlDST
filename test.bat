f273f00cf50c5b0d8072db7d30b739d79067d6f1
Mike Mitchell <mmitchell@ducks.org>
2021-02-22 09:50:02 -0500

    Update test.bat
    Added a few parameters that have been missing.

--- test.bat
+++ test.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-python runModel.py -w "path to workspace where geodatabase and csvs are kept" -g Geodatabase_name.gdb -l NWILayer_name NWI_crossover.json -p PADUS_name -n NCED_name -k kcal_table.csv -d EnergyDemand_layer_name -e Extradataset1_layer_name Extradataset_crossover.csv -a AreaofInterest_name -b Bin_layer_name
+python runModel.py -w "path to workspace where geodatabase and csvs are kept" -g Geodatabase_name.gdb -l NWILayer_name NWI_crossover.json -p PADUS_name -n NCED_name -k kcal_table.csv -d EnergyDemand_layer_name -e Extradataset1_layer_name Extradataset_crossover.csv -a AreaofInterest_name -b Bin_layer_nameu -u Unique Bin field -r Urban feature class -z debug 1 1 1 1 1 1 1
