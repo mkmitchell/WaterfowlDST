0dc268b65abb2f2d93878838c761dd152bdafce7
jeskill <jes.skillman@gmail.com>
2021-03-01 12:27:09 -0500

    edited copyright date

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,8 +18,8 @@ sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'ABDU DST'
-copyright = '2019, Ducks Unlimited'
+project = 'American Black Duck Habitat Priorities'
+copyright = '2020, Ducks Unlimited'
 author = 'Ducks Unlimited'
 
 # The full version, including alpha/beta/rc tags
