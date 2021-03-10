66d8488bb74ff64aa2d578575c41db7eef613c1e
Mike Mitchell <mmitchell@ducks.org>
2021-03-10 10:56:09 -0500

    Update test.bat
    Updating example call

--- test.bat
+++ test.bat
@@ -1,2 +1,3 @@
 @ECHO OFF
-python runModel.py -w "path to workspace where geodatabase and csvs are kept" -g Geodatabase_name.gdb -l NWILayer_name NWI_crossover.json -p PADUS_name -n NCED_name -k kcal_table.csv -d EnergyDemand_layer_name -e Extradataset1_layer_name Extradataset_crossover.csv -a AreaofInterest_name -b Bin_layer_nameu -u Unique Bin field -r Urban feature class -z debug 1 1 1 1 1 1 1
+::[Supply, Demand, species proportion, protected lands, habitat proportion, weighted mean, data check, zip it]
+python runModel.py -w "path to workspace where geodatabase and csvs are kept" -g Geodatabase_name.gdb -l NWILayer_name NWI_crossover.json -p PADUS_name -n NCED_name -k kcal_table.csv -d EnergyDemand_layer_name -e Extradataset1_layer_name Extradataset_crossover.csv -a AreaofInterest_name -b Bin_layer_name -u Unique_Bin_field Bin_name -r Urban_feature_class -f Field_Table -z debug 1 1 1 1 1 1 1 1
