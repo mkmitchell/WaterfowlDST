58693697e9cab1d691fcfd135a460b2a373a0a7e
Michael Mitchell <mmitchell@ducks.org>
2022-09-19 12:52:45 -0400

    Cleaning up files

--- southMS2.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-::[Supply, demand, species proportion, protected lands, habitat proportion, urban, data check, zip it]
-python runModel.py -w "C:\Proc\Mike\JVDST" -g ModelReady.gdb -l CONUS_wetlands aoiWetland.json -p PADUS2_0Fee -n NCED_Polygons -k SAkcal.csv -d Demand9Species_Merged -e MarshVector marsh.csv Impoundments Impoundments.csv -a southMS2 -b WBDHU12 -u HUC12 name -r SouthMSUrbanDissolve -f ModelOutputFieldDictionary.csv -z 1 1 1 1 1 1 1 1 1 1
